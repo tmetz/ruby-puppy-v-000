3cef612544c2d515228289a7a958d1fc03f061f6
Tammy Metz <tammy@tammymetz.com>
2018-08-09 15:42:13 +0000

    automatically backed up by learn

---
+++ lib/dog.rb
@@ -0,0 +1,21 @@
+class Dog
+  @@all = []
+
+  attr_reader :name
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all.each do |dog|
+      puts dog
+    end
+  end
+
+  def self.clear_all
+    @@all.clear
+  end
+
+end
